baa2c052a0e5c26a7caac66d6fd41719bb6bf537
Offbirk <regimenbria@gmail.com>
2024-11-25 17:47:19 -0500

    Formulario reservas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const obtenerFechaMinima = () => {
+    const hoy = new Date();
+    hoy.setHours(8, 0, 0, 0); //Establece la hora a las 8:00
+    return hoy.toISOString().slice(0, 16);
+  }
+
+  const obtenerFechaMaxima = () => {
+    const hoy = new Date();
+    hoy.setHours(16, 0, 0, 0); //Establece la hora a las 16:00
+    return hoy.toISOString().slice(0, 16);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <nav className='navbar navbar-expan-lg navbar-light bg-light'>
+        <a className='navbar-brand' href='#'>NIDO</a>
+        <a className='navbar-brand' href='#'>Inicio</a>
+        <a className='navbar-brand' href='#'>Reservas</a>
+      </nav>
+      <div className='container mt-4'>
+        <h1>Registro de salas para la empresa NIDO</h1>
+        <input type='email' className='form-control' placeholder='Usuario_ID' required/>
+        <input type='number' className='form-control' placeholder='Sala_ID' min={1} max={1} required/>
+        <label>Fecha de inicio de la reserva</label>
+        <input type='datetime-local' className='form-control' placeholder='Fecha de inicio' min={obtenerFechaMinima()} max={obtenerFechaMaxima()} required/>
+        <label>Fecha de final de la reserva</label>
+        <input type='datetime-local' className='form-control' placeholder='Fecha de final' min={obtenerFechaMinima()} max={obtenerFechaMaxima()} required/>
+        <button type='submit' class="btn btn-primary">Enviar</button>        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ frontend/src/Salones.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+function Salones() {
+    const [salon, setSalon] = useState(null);
+    return (
+        <div>
+            <h1>Salones 1-5</h1>
+        </div>
+    )
+}
+
+export default Salones
