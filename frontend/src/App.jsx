fc0af4ebc9f370b2116ec9d38f9494690031e9b9
Jjuan1302 <jjuan1302@hotmail.com>
2024-11-26 10:51:33 -0500

    navbar, cards

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
-import Reservas from './Reservas';
-import Usuario from './Usuario';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './App.css';
+import './style.css';
+import Salones from './salones';
+
 
 function App() {
-
+  
   const obtenerFechaMinima = () => {
     const hoy = new Date();
     hoy.setHours(8, 0, 0, 0); //Establece la hora a las 8:00
@@ -20,15 +22,81 @@ function App() {
 
   return (
     <>
-      <nav className='navbar navbar-expan-lg navbar-light bg-light'>
-        <a className='navbar-brand' href='#'>NIDO</a>
-        <a className='navbar-brand' href='#'>Inicio</a>
-        <a className='navbar-brand' href='#'>Reservas</a>
+    {/* navbar */}
+      <header className="main-header">
+      <nav className="navbar header-nav navbar-expand-lg">
+        <div className="container">
+          {/* Logo */}
+          <a href="#" className="navbar-brand">
+            <img src="src/assets/img/logo.png" height="100px" className="rounded-pill" alt="logo" />
+          </a>
+
+          {/* Menu */}
+          <div className="collapse navbar-collapse justify-content-end" id="navbar-collapse-toggle">
+            <ul className="navbar-nav mx-auto">
+              <li>
+                <a href="#" className="nav-link">
+                  Inicio
+                </a>
+              </li>
+              <li className="ms-5">
+                <a href="#" className="nav-link">
+                  Reservas
+                </a>
+              </li>
+              <li className="ms-5">
+                <a href="#" className="nav-link">
+                  Mis reservas
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          {/* Botones */}
+          <div className="ms-auto me-2 d-none d-lg-block">
+            <a className="color-btn" href="contacto.html">
+              INICIO SESION
+            </a>
+          </div>
+
+          <div className="ms-auto d-none d-lg-block">
+            <a className="color-btn" href="contacto.html">
+              REGISTRO
+            </a>
+          </div>
+
+          {/* Botón desplegable */}
+          <button
+            className="navbar-toggler collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbar-collapse-toggle"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
       </nav>
-      <Reservas />
-      <Usuario />
+    </header>
+
+    <Salones/>
+
+      {/* formulario */}
+      <div className='container mt-4'>
+        <h1>Registro de salas para la empresa NIDO</h1>
+        <input type='email' className='form-control' placeholder='Usuario_ID' required/>
+        <input type='number' className='form-control' placeholder='Sala_ID' min={1} max={1} required/>
+        <label>Fecha de inicio de la reserva</label>
+        <input type='datetime-local' className='form-control' placeholder='Fecha de inicio' min={obtenerFechaMinima()} max={obtenerFechaMaxima()} required/>
+        <label>Fecha de final de la reserva</label>
+        <input type='datetime-local' className='form-control' placeholder='Fecha de final' min={obtenerFechaMinima()} max={obtenerFechaMaxima()} required/>
+        <button type='submit' class="btn btn-primary">Enviar</button>        
+      </div>
+      
     </>
   )
 }
 
 export default App
+
--- frontend/src/Salones.jsx
+++ frontend/src/Salones.jsx
@@ -1,12 +1,48 @@
-import { useState } from "react"
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import sala1 from "./assets/img/sala1.jpg"
+import sala2 from "./assets/img/sala2.jpg"
+import sala3 from "./assets/img/sala3.jpg"
+import sala4 from "./assets/img/sala4.jpg"
+import sala5 from "./assets/img/sala5.jpg"
 
 function Salones() {
-    const [salon, setSalon] = useState(null);
-    return (
-        <div>
-            <h1>Salones 1-5</h1>
-        </div>
-    )
+  const imagenes = [sala1, sala2, sala3, sala4, sala5];
+  
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        {imagenes.map((imagen, index) => (
+          <div className="col-md-4 mb-4" key={index}>
+            <div className="card">
+              <img 
+                className="card-img-top" 
+                src={imagen} 
+                alt={`Sala ${index + 1}`} 
+                height={300}
+              />
+              <div className="card-body">
+                <h1 className="card-title">Sala {index + 1}</h1>
+                <p className="card-subtitle">
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </p>
+                <div className="d-grid gap-2">
+                  <button className="btn btn-primary my-2 p-3" type="button">
+                    Reservar
+                  </button>
+                </div>
+                <div className="alert alert-success" role="alert">
+                  Disponible
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Salones
+export default Salones;
