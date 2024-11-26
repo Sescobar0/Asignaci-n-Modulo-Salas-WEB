425da21d8b53d8cda7983cbef8f8231a443b744e
Offbirk <regimenbria@gmail.com>
2024-11-26 10:40:53 -0500

    Formularios Reservas, Usuario

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import Reservas from './Reservas';
+import Usuario from './Usuario';
 
 function App() {
 
@@ -23,16 +25,8 @@ function App() {
         <a className='navbar-brand' href='#'>Inicio</a>
         <a className='navbar-brand' href='#'>Reservas</a>
       </nav>
-      <div className='container mt-4'>
-        <h1>Registro de salas para la empresa NIDO</h1>
-        <input type='email' className='form-control' placeholder='Usuario_ID' required/>
-        <input type='number' className='form-control' placeholder='Sala_ID' min={1} max={1} required/>
-        <label>Fecha de inicio de la reserva</label>
-        <input type='datetime-local' className='form-control' placeholder='Fecha de inicio' min={obtenerFechaMinima()} max={obtenerFechaMaxima()} required/>
-        <label>Fecha de final de la reserva</label>
-        <input type='datetime-local' className='form-control' placeholder='Fecha de final' min={obtenerFechaMinima()} max={obtenerFechaMaxima()} required/>
-        <button type='submit' class="btn btn-primary">Enviar</button>        
-      </div>
+      <Reservas />
+      <Usuario />
     </>
   )
 }
